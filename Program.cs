4193ce0059447e1b5eed2fb350f100f008e07d32
Didcvee <8anibus8@gmail.com>
2025-04-02 20:05:10 +0300

    [#main] first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace HelloWorldApp;
+
+public class Program
+{
+    private static int SumDigits(BigInteger number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += (int)(number % 10);
+            number /= 10;
+        }
+        return sum;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите основание степени: ");
+        int baseNumber = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Введите показатель степени: ");
+        int exponent = int.Parse(Console.ReadLine() ?? "0");
+        
+        BigInteger powerResult = BigInteger.Pow(baseNumber, exponent);
+        int digitSum = SumDigits(powerResult);
+        
+        Console.WriteLine($"Сумма цифр числа {baseNumber}^{exponent} ({powerResult}) = {digitSum}");
+    }
+}
